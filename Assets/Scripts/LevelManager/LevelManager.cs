8e288a7ae464f1fb5477439917bb5eed51711351
BCGReaver <aaguilera39932@ucq.edu.mx>
2025-07-18 02:58:01 -0600

    ArturoGod

---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pause;
+    private bool isPaused;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if(isPaused == false)
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0.0f;
+            isPaused = true;
+        }
+        else
+        {
+            pause.SetActive(false);
+            Time.timeScale = 1.0f;
+            isPaused = false;
+        }
+    }
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
